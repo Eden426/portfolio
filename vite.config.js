4ef9b24812bdd31261c6b9fc5870edb8b57753de
Eden Nigatu <edennigatu65@gmail.com>
2026-03-23 19:16:27 +0300

    fix: smart base path for github and vercel

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,17 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/portfolio/", // Add this line - use your exact repo name
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+// https://vite.dev
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [react(), tailwindcss()],
+    // If we are deploying to GitHub (production mode), use /portfolio/
+    // Otherwise (Vercel or Local), use /
+    base: mode === "production" ? "/portfolio/" : "/",
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
 });
