dde7e938003eb7c11db116e63a232ec44e03ed91
Eden Nigatu <edennigatu65@gmail.com>
2026-03-23 19:20:43 +0300

    fix: environment-based base path

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,12 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vite.dev
 export default defineConfig(({ command, mode }) => {
   return {
     plugins: [react(), tailwindcss()],
-    // If we are deploying to GitHub (production mode), use /portfolio/
-    // Otherwise (Vercel or Local), use /
-    base: mode === "production" ? "/portfolio/" : "/",
+    // If we are on GitHub Actions (deploying to GitHub), use /portfolio/
+    // If we are on Vercel or Local, use /
+    base: process.env.GITHUB_ACTIONS === "true" ? "/portfolio/" : "/",
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
